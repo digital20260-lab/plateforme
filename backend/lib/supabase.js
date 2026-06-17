5c630a536a79ec7dee26a75c4918035132eee885
patrickgit559 <patrickmiante@outlook.fr>
2026-06-17 09:36:42 +0000

    fix: export supabaseAdmin from supabase.js

--- backend/lib/supabase.js
+++ backend/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import ws from 'ws'
 
-export const supabase = createClient(
+export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
